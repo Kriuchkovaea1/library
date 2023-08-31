aa50fc640e809c8406749da2d162c69b8361d3f6
Kriuchkovaea1 <79831087227@yandex.ru>
2023-08-31 16:20:03 +0700

    Factories & Seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Author::factory(10)->create();
+        $genres = Genre::factory(15)->create();
+        $books = Book::factory(10)->create();
+
+        foreach ($books as $book){
+            $bookIds = $genres->random(5)->pluck('id');
+            $book->genres()->attach($bookIds);
+        }
     }
 }
